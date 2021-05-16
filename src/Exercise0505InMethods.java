022181a6ddfcc7be316509d7b805759b312378d5
Csaba79-coder <csabavadasz79@gmail.com>
2021-05-16 22:56:24 +0200

    refactor and solution of other tasks - convert into methods

--- src/Exercise0505InMethods.java
+++ src/Exercise0505InMethods.java
@@ -13,6 +13,9 @@ public class Exercise0505InMethods {
 
         getAverageOfNums3();
 
+        greetingUser();
+
+        basicArithmeticOfTwoNums();
     }
 
 
@@ -27,45 +30,51 @@ public class Exercise0505InMethods {
     }
 
 
-    public static int getFirstNumInt() {
+    public static int getNumInt() {
         return getInput();
     }
 
 
-    public static double getFirstNumDouble() {
-        return getInput();
-    }
-
-
-    public static int getSecondNumInt() {
-        return getInput();
-    }
-
-
-    public static double getSecondNumDouble() {
-        return getInput();
-    }
-
-
-    public static double getThirdNumDouble() {
+    public static double getNumDouble() {
         return getInput();
     }
 
 
     public static void remainderWithoutModulo() {
-        int firstNum = getFirstNumInt();
-        int secondNum = getSecondNumInt();
+        int firstNum = getNumInt();
+        int secondNum = getNumInt();
         int modulo = firstNum - secondNum * (firstNum / secondNum);
         System.out.println("The result (using without modulo) is: " + modulo);
     }
 
 
     public static void getAverageOfNums3 () {
-        double firstNum = getFirstNumDouble();
-        double secondNum = getSecondNumDouble();
-        double thirdNum = getThirdNumDouble();
+        double firstNum = getNumDouble();
+        double secondNum = getNumDouble();
+        double thirdNum = getNumDouble();
         double average = ((firstNum + secondNum + thirdNum) / 3);
         System.out.println("The average of the 3 nums are: " + average);
+    }
+
+    public static String getUserName() {
+        System.out.println("Please enter your name: ");
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+
+
+    public static void greetingUser() {
+        System.out.println("Hello dear user: " + getUserName());
+    }
+
+
+    public static void basicArithmeticOfTwoNums() {
+        double firstNum = getNumInt();
+        double secondNum = getNumInt();
+        System.out.println("The sum of the nums: " + (firstNum + secondNum));
+        System.out.println("The difference of the nums: " + (firstNum - secondNum));
+        System.out.println("The multiple of the nums: " + (firstNum * secondNum));
+        System.out.println("The division of the nums: " + (firstNum / secondNum));
     }
 }
 
