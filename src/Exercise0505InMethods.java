5a2c3bc1a882a070775f198023801f37e803a1a0
Csaba79-coder <csabavadasz79@gmail.com>
2021-05-16 12:16:27 +0200

    remainder division is created with methods without using modulo operator

--- src/Exercise0505InMethods.java
+++ src/Exercise0505InMethods.java
@@ -8,7 +8,9 @@ public class Exercise0505InMethods {
     public static void main(String[] args) {
 
         scanner = getScanner();
-        System.out.println(getInput());
+
+        remainderWithoutModulo();
+
 
     }
 
@@ -22,17 +24,25 @@ public class Exercise0505InMethods {
         System.out.println("Please enter a number: ");
         return scanner.nextInt();
     }
+
+
+    public static int getFirstNum() {
+        return getInput();
+
+    }
+
+
+    public static int getSecondNum() {
+        return getInput();
+    }
+
+
+    public static void remainderWithoutModulo() {
+        int firstNum = getFirstNum();
+        int secondNum = getSecondNum();
+        int modulo = firstNum - secondNum * (firstNum / secondNum);
+        System.out.println("The result (using without modulo) is: " + modulo);
+    }
 }
-
-
-//
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Please give the first whole number: ");
-//        int a = sc.nextInt();
-//        System.out.println("Please give the second whole number: ");
-//        int b = sc.nextInt();
-//
-//        // % without using modulo operator
-//        System.out.println(a - b * (a / b));
 
 
