10b9b530e143d7049b10eefa3fab772fa42f2518
Csaba79-coder <csabavadasz79@gmail.com>
2021-05-16 20:32:29 +0200

    different pyramid logics different figure/illustration/graph/pic

--- src/ArraysIfLoops_0508_03_ThirdPart_DrawingPyramidsFromStars_InMethods.java
+++ src/ArraysIfLoops_0508_03_ThirdPart_DrawingPyramidsFromStars_InMethods.java
@@ -9,8 +9,13 @@ public class ArraysIfLoops_0508_03_ThirdPart_DrawingPyramidsFromStars_InMethods 
 
         firstPyramid();
 
+        System.out.println("------------------------");
+
         secondPyramid();
 
+        System.out.println("------------------------");
+
+        thirdPyramid();
 
     }
 
@@ -25,9 +30,26 @@ public class ArraysIfLoops_0508_03_ThirdPart_DrawingPyramidsFromStars_InMethods 
     }
 
     public static void secondPyramid() {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j) {
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = (n - i); j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void thirdPyramid() {
+        for (int i=0; i<n; i++) {
+            for (int j=n-i; j>1; j--) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++ ) {
+                System.out.print("* ");
             }
             System.out.println();
         }
--- src/Pyramid.java
+++ src/Pyramid.java
@@ -8,14 +8,25 @@ public class Pyramid {
 
         firstPyramid();
 
+        secondPyramid();
+
         firstWithSpacesPyramid();
 
         getSquare();
+
+        getSquareWithSpaces();
 
         differentPyramid();
 
         extraSizePyramid();
 
+        strangePyramid();
+
+        secondNotBadPyramid();
+
+        thirdPyramidWithSpaces();
+
+        anotherPyramid();
     }
 
 
@@ -49,6 +60,16 @@ public class Pyramid {
     }
 
 
+    public static void getSquareWithSpaces() {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void differentPyramid() {
         int k = 0;
         for (int i = 1; i <= n; ++i, k = 0) {
@@ -73,6 +94,100 @@ public class Pyramid {
             while (k != 2 * i - 1) {
                 System.out.print("* ");
                 ++k;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void strangePyramid() {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; ++k) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void secondNotBadPyramid() {
+        int i, j;
+        // outer loop to handle number of rows
+        //  n in this case
+        for (i = 0; i < n; i++) {
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (j = 2 * (n - i); j >= 0; j--) {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (j = 0; j <= i; j++) {
+                // printing stars
+                System.out.print(" *");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
+    }
+
+
+    public static void secondPyramid() {
+        int i, j;
+        for(i=0; i<n; i++) {
+            for(j=(n-i); j>=0; j--)
+            {
+                System.out.print(" ");
+            }
+            for(j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void thirdPyramidWithSpaces() {
+        // outer loop to handle number of rows
+        //  n in this case
+        for (int i=0; i<n; i++)
+        {
+
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (int j=n-i; j>1; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (int j=0; j<=i; j++ )
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
+    }
+
+
+    public static void anotherPyramid() {
+        for (int i=0; i<n; i++) {
+            for (int j=n-i; j>1; j--) {
+                System.out.print("");
+            }
+            for (int j=0; j<=i; j++ ) {
+                System.out.print("* ");
             }
             System.out.println();
         }
