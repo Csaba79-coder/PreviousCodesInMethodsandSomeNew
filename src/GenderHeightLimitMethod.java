04e0b2f8ffab8bcbcdbba56b39ecaadcb0aeecda
Csaba79-coder <csabavadasz79@gmail.com>
2021-05-24 22:49:27 +0200

    creating GenderHeightLimitMethod.java

---
+++ src/GenderHeightLimitMethod.java
@@ -0,0 +1,12 @@
+public class GenderHeightLimitMethod {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void String(int number) {
+        if (number / 2 == 3);
+    }
+}
